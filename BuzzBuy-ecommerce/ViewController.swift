6797ec16d4c3372010b26a702f6ffab2fd0200de
Robert <robert.charca@tecsup.edu.pe>
2023-06-10 12:36:07 -0500

    Initial Commit

---
+++ BuzzBuy-ecommerce/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuzzBuy-ecommerce
+//
+//  Created by Robert Charca on 10/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
